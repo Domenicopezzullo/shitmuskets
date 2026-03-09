489ec030f8128148159932d195178923a776f341
Domenicopezzullo <pezzullodomenico712@gmail.com>
2026-03-09 18:38:36 +0100

    Initial commit

---
+++ src/main/kotlin/com/domenico/shitmuskets/ShitmusketsDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.domenico.shitmuskets
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ShitmusketsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
